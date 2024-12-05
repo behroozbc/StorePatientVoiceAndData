826d30bf23b8f9c1f144048b01f97225bc96a81c
behrooz bozorg chami <behroozbc@outlook.com>
2024-12-05 18:32:41 +0100

    change project structure

--- StorePatientVoiceAndData.AppHost/Program.cs
+++ StorePatientVoiceAndData.AppHost/Program.cs
@@ -1,10 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.StorePatientVoiceAndData_ApiService>("apiservice");
-
-builder.AddProject<Projects.StorePatientVoiceAndData_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService)
-    .WaitFor(apiService);
+builder.AddProject<Projects.StorePatientVoiceAndData>("storepatientvoiceanddata");
 
 builder.Build().Run();
